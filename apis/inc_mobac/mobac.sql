0fbe5a5a5c4a33145cc5f8f6a94dde8258c1a2e5
rahul lahoria <rahul_lahoria@yahoo.com>
2016-02-16 15:07:50 +0530

    db contacts schema update

--- apis/inc_mobac/mobac.sql
+++ apis/inc_mobac/mobac.sql
@@ -48,16 +48,15 @@ CREATE TABLE IF NOT EXISTS `call_details` (
 
 CREATE TABLE IF NOT EXISTS `contacts` (
   `id` int(20) NOT NULL AUTO_INCREMENT,
-  `user_id` int(14) NOT NULL,
-  `first_name` varchar(20) NOT NULL,
-  `last_name` varchar(20) NOT NULL,
-  `number` varchar(160) NOT NULL,
+  `client_id` int(14) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `mobile` varchar(16) NOT NULL,
   `email` varchar(160) NOT NULL,
-  `time` datetime NOT NULL,
-  `type` enum('Inbox','Sent') NOT NULL,
+  `creation` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `status` int(2) NOT NULL DEFAULT '0',
+  `location` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
 
 -- --------------------------------------------------------
 
