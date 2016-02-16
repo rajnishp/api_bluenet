88ed348b15a0f86012b6c9ac1143d78b9df66258
rahul lahoria <rahul_lahoria@yahoo.com>
2016-02-16 15:46:04 +0530

    added alow

--- apis/inc_reg/reg.php
+++ apis/inc_reg/reg.php
@@ -1,7 +1,7 @@
 <?php
 
 	$input = json_decode(file_get_contents ("php://input"));
-	
+	var_dump($input);
 
 	$sql = "INSERT INTO bluenet_v0.clients (id, name, email, password, mobile, location) 
 				VALUES (NULL, 
@@ -9,7 +9,7 @@
 					'". $input->root->email."', 
 					'". $input->root->password."', 
 					'". $input->root->mobile."', 
-					'". $input->root->location."');";
+					'". $input->root->gps_location."');";
 
 	$user = mysqli_query ($db_handle, $sql);
 	if(mysqli_connect_errno()){
