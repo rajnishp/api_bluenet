0c4e5a6c816f786de8cedb84dcf593cd7ff46951
rahullahoria <rahul_lahoria@yahoo.com>
2016-03-07 17:36:50 +0530

    get work, post work res added

--- apis/app_rec/getWork.php
+++ apis/app_rec/getWork.php
@@ -21,29 +21,23 @@ $currentTime15 = date('H:i:s', strtotime($_GET["current_time"]) + 900);
     . "WHERE wcm.user_worker_id ="
     . "AND t.start_time > '$currentTime' and t.start_time < '$currentTime15';";*/
 
-$sql = "SELECT wcm.user_worker_id, wcm.service_request_id, t.start_time, t.end_time,u.name as customer_name,u.mobile as customer_mobile, u.address as customer_address\n"
-    . "FROM `bluenet_v3`.worker_customer_match AS wcm\n"
-    . "INNER JOIN `bluenet_v3`.timings AS t\n"
-    . "INNER JOIN `bluenet_v3`.service_request AS sr\n"
-    . "INNER JOIN `bluenet_v3`.users AS u\n"
-    . "WHERE wcm.user_worker_id =$workerId \n"
-    . "AND t.start_time > \'$currentTime\'\n"
-    . "AND t.start_time < \'$currentTime15\'\n"
-    . "AND t.service_request_id = sr.id\n"
+$sql = "SELECT wcm.user_worker_id, wcm.service_request_id, t.start_time, t.end_time,u.name as customer_name,u.mobile as customer_mobile, u.address as customer_address "
+    . "FROM `bluenet_v3`.worker_customer_match AS wcm "
+    . "INNER JOIN `bluenet_v3`.timings AS t "
+    . "INNER JOIN `bluenet_v3`.service_request AS sr "
+    . "INNER JOIN `bluenet_v3`.users AS u "
+    . "WHERE wcm.user_worker_id =$workerId "
+    . "AND t.start_time > '$currentTime' "
+    . "AND t.start_time < '$currentTime15' "
+    . "AND t.service_request_id = sr.id "
     . "AND sr.user_id = u.id";
 
-//echo $sql;
+echo $sql;
 
 $work = mysqli_query($db_handle, $sql);
 
 //var_dump(mysqli_fetch_assoc($work));
 
-if (mysqli_connect_errno()) {
-    /* send 500 html header*/
-    internalServerError("Error description: " . mysqli_error($db_handle));
-    echo("Error description: " . mysqli_error($db_handle));
-    die();
-}
 
 echo "{\"root\":{\"work\":";
 print json_encode(mysqli_fetch_assoc($work));
