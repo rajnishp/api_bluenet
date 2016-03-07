ddca000aeee3c7e676042c748e8d0f344252b7f1
rahullahoria <rahul_lahoria@yahoo.com>
2016-03-07 17:07:23 +0530

    get work, post work res added

--- apis/app_rec/getWork.php
+++ apis/app_rec/getWork.php
@@ -15,7 +15,7 @@ $workerId = $route[2];
 $currentTime = date('H:i:s', strtotime($_GET["current_time"]));
 $currentTime15 = date('H:i:s', strtotime($_GET["current_time"]) + 900);
 
-$sql = "SELECT user_worker_id, service_request_id "
+$sql = "SELECT wcm.user_worker_id, wcm.service_request_id "
     . "FROM worker_customer_match AS wcm "
     . "INNER JOIN timings AS t "
     . "WHERE wcm.user_worker_id =$workerId "
