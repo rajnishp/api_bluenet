95d92ead185aaabee0c47aebd5ab8a6e31697a63
rahullahoria <rahul_lahoria@yahoo.com>
2016-03-14 10:31:59 +0530

    tc

--- apis/app_rec/get_sr_by_mobile.php
+++ apis/app_rec/get_sr_by_mobile.php
@@ -13,18 +13,14 @@ $input = json_decode(file_get_contents("php://input"));
 
 $result = mysqli_query($db_handle, "SELECT  *  FROM `bluenet_v3`.`service_request` as sr"
     . "INNER JOIN `bluenet_v3`.timings AS t "
-    ." WHERE sr.mobile = '" . $route[2]
+    ." WHERE sr.mobile = '" . $route[2]  ."'"
     . "AND sr.id = t.service_request_id "
-    . "'; ");
+    . "; ");
 
 $details = mysqli_fetch_assoc($result);
-if (mysqli_num_rows($result) >= 1)
-    $details['user_exist'] = true;
-else
-    $details['user_exist'] = false;
 
 
-echo "{\"root\":{\"user\":";
+echo "{\"root\":{\"srs\":";
 print json_encode($details);
 echo "}}";
 
