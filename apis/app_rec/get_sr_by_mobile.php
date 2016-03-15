409977c5c522ef8de5249a666df1a4b15d552522
rahullahoria <rahul_lahoria@yahoo.com>
2016-03-15 15:55:39 +0530

    sr cem details

--- apis/app_rec/get_sr_by_mobile.php
+++ apis/app_rec/get_sr_by_mobile.php
@@ -17,12 +17,12 @@ $input = json_decode(file_get_contents("php://input"));
 . "AND sr.id = t.service_request_id "
 . "; ";*/
 
-$sql = "SELECT sr.id, mobile,service,service_type,salary,user_cem_id,start_time,end_time, u.id as cem_id, u.name as cem_name, u.mobile as cem_mobile  "
+$sql = "SELECT sr.id, service,service_type,salary,user_cem_id,start_time,end_time, u.id as cem_id, u.name as cem_name, u.mobile as cem_mobile  "
     . "FROM `bluenet_v3`.`service_request` AS sr "
     . "INNER JOIN `bluenet_v3`.timings AS t "
     . "INNER JOIN `bluenet_v3`.users AS u "
     . "WHERE sr.mobile = '" . $route[2]  ."' "
-    . "AND sr.id = t.service_request_id"
+    . "AND sr.id = t.service_request_id "
     . "AND sr.user_cem_id = u.id";
 
 $result = mysqli_query($db_handle, $sql);
