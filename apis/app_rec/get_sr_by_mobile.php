d361e13133d714dcda34954bbe773baf60a043b0
rahullahoria <rahul_lahoria@yahoo.com>
2016-03-18 19:51:13 +0530

    added ratings 4

--- apis/app_rec/get_sr_by_mobile.php
+++ apis/app_rec/get_sr_by_mobile.php
@@ -17,25 +17,19 @@ $input = json_decode(file_get_contents("php://input"));
 . "AND sr.id = t.service_request_id "
 . "; ";*/
 
-$sql = "SELECT sr.id, service,service_type,salary,start_time,end_time,"
+$sql = "SELECT sr.id, service,service_type,salary,start_time,end_time, "
     ." u.id as cem_id, u.name as cem_name, u.mobile as cem_mobile, r.rating as cem_rating,  "
     ." uw.id as worker_id, uw.name as worker_name, uw.mobile as worker_mobile, wr.rating as worker_rating  "
     . "FROM `bluenet_v3`.`service_request` AS sr "
-    . "INNER JOIN `bluenet_v3`.timings AS t "
-    . "INNER JOIN `bluenet_v3`.worker_customer_match AS wcm "
-    . "INNER JOIN `bluenet_v3`.ratings AS r "
-    . "INNER JOIN `bluenet_v3`.ratings AS wr "
-    . "INNER JOIN `bluenet_v3`.users AS u "
-    . "INNER JOIN `bluenet_v3`.users AS uw "
-    . "WHERE sr.mobile = '" . $route[2]  ."' "
-    . "AND sr.id = wcm.service_request_id "
-    . "AND wcm.user_worker_id = uw.id "
-    . "AND wcm.user_worker_id = wr.user_id "
-    . "AND sr.user_id = wr.customer_user_id "
-    . "AND wcm.type != 'leave' "
-    . "AND sr.id = t.service_request_id "
-    . "AND sr.user_cem_id = u.id "
-    . "AND sr.user_cem_id = r.user_id ";
+    . "LEFT JOIN `bluenet_v3`.worker_customer_match AS wcm ON sr.id = wcm.service_request_id AND wcm.type != 'leave' "
+    . "LEFT JOIN `bluenet_v3`.users AS uw ON wcm.user_worker_id = uw.id  "
+    . "LEFT JOIN `bluenet_v3`.timings AS t  ON sr.id = t.service_request_id "
+
+    . "LEFT JOIN `bluenet_v3`.ratings AS r ON sr.user_cem_id = r.user_id "
+    . "LEFT JOIN `bluenet_v3`.ratings AS wr ON wcm.user_worker_id = wr.user_id AND sr.user_id = wr.customer_user_id "
+    . "LEFT JOIN `bluenet_v3`.users AS u sr.user_cem_id = u.id "
+
+    . "WHERE sr.mobile = '" . $route[2]  ."' ";
 
 $result = mysqli_query($db_handle, $sql);
 echo $sql;
