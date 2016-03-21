bfd93f9b59b0e1404316e32b62e20f8e2c5a3fdf
rahullahoria <rahul_lahoria@yahoo.com>
2016-03-21 13:29:41 +0530

    update rating 2

--- apis/app_rec/updateRating.php
+++ apis/app_rec/updateRating.php
@@ -7,16 +7,14 @@
  */
 $input = json_decode(file_get_contents("php://input"));
 
-$sql = "INSERT INTO `bluenet_v3`.`payments`
-                      (`id`, `service_request_id`, `amount`, `user_cem_id`, `device_id`, `gps_location`, `user_id`, `check_no`)
-                      VALUES (NULL,
-                      '" . $input->root->sr_id . "',
-                      '" . $input->root->amount . "',
-                       '" . $input->root->user_id . "',
-                       '" . $input->root->device_id . "',
-                        '" . $input->root->gps_location . "',
-                          '" . $input->root->customer_id . "',
-                          '" . $input->root->check_no . "');";
+//
+
+$sql = "INSERT INTO `bluenet_v3`.`ratings` (`id`, `user_id`, `customer_user_id`, `rating`) VALUES
+            (NULL,
+             '" . $input->root->user_id . "',
+             '" . $input->root->customer_user_id . "',
+             '" . $input->root->rating . "') ON DUPLICATE KEY UPDATE rating = '" . $input->root->rating . "' ;";
+
 $result = mysqli_query($db_handle, $sql);
 
 $input->root->id = mysqli_insert_id($db_handle);
