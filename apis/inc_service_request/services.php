2ca4ab45fe8f58081824829bfaa113b3c09234c0
rahullahoria <rahul_lahoria@yahoo.com>
2016-03-30 14:49:43 +0530

    added api type=monthly

--- apis/inc_service_request/services.php
+++ apis/inc_service_request/services.php
@@ -4,16 +4,29 @@
 	//var_dump($input);
 
 	//$user_id = $_SESSION['user_id'];
-	
-	$services = mysqli_query($db_handle, "SELECT * FROM `bluenet_v0`.`services` WHERE status = 1 ORDER BY `services`.`priority` ASC ; ") ;
 
-	for($servicesArr = array(); $service = mysqli_fetch_assoc($services); $servicesArr[] = $service);
+	if(!isset($_GET['type'])) {
 
-	for ($i=0; $i < count($servicesArr) ; $i++) 
-		$servicesArr[$i]["plans"] = json_decode($servicesArr[$i]["plans"]);
+		$services = mysqli_query($db_handle, "SELECT * FROM `bluenet_v0`.`services` WHERE status = 1 ORDER BY `services`.`priority` ASC ; ");
 
-	echo "{\"root\":";
-	print (json_encode($servicesArr));
-	echo "}";
+		for ($servicesArr = array(); $service = mysqli_fetch_assoc($services); $servicesArr[] = $service) ;
+
+		for ($i = 0; $i < count($servicesArr); $i++)
+			$servicesArr[$i]["plans"] = json_decode($servicesArr[$i]["plans"]);
+
+		echo "{\"root\":";
+		print (json_encode($servicesArr));
+		echo "}";
+	} else {
+
+		$services = mysqli_query($db_handle, "SELECT DISTINCT `service` FROM `bluenet_v3`.`plans` WHERE service_type = 'monthly';");
+
+		for ($servicesArr = array(); $service = mysqli_fetch_assoc($services); $servicesArr[] = $service) ;
+
+		echo "{\"root\":";
+		print (json_encode($servicesArr));
+		echo "}";
+
+	}
 
 ?>
