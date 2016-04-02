83b60f1eed58bdc2197cccf4df147409fde05ba4
rahullahoria <rahul_lahoria@yahoo.com>
2016-04-02 21:00:46 +0530

    get score api

--- apis/inc_workers/worker.php
+++ apis/inc_workers/worker.php
@@ -1,8 +1,8 @@
 <?php
 
-$sql = "SELECT *
-FROM `bluenet_v3`.`workers`
-WHERE `ref_id` =" . $_GET['user_id'];
+$sql = "SELECT u.id as id, u.name, u.mobile, w.id as worker_id , w.emergency_no
+FROM `bluenet_v3`.`workers` as w inner join `bluenet_v3`.users as u
+WHERE w.`ref_id` =" . $_GET['user_id'] ." and w.user_id = u.id  ";
 
 $result = mysqli_query($db_handle, $sql);
 //echo $sql;
