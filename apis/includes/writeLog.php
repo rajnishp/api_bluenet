f6326bed7ec5e09bca82a421ecbcdd7d1bfe0ed1
rahullahoria <rahul_lahoria@yahoo.com>
2016-05-31 00:48:19 +0530

    added loger

--- apis/includes/writeLog.php
+++ apis/includes/writeLog.php
@@ -6,16 +6,22 @@
  * Time: 4:42 PM
  */
 
+
+
 $input = json_decode(file_get_contents ("php://input"));
 $header = json_encode($_SERVER);
 $url = $_SERVER[REQUEST_URI];
+$timeTaken = time()-$scriptStartTime;
+$os = getOS();
 
-
-$sql = "INSERT INTO `request_logs`.`raw_logs` (id, input, header, url)
+$sql = "INSERT INTO `request_logs`.`raw_logs` (id, input, header, url,time_taken,ip,os)
 				VALUES (NULL,
 					'".	json_encode($input)."',
 					'". $header."',
-					'". $url."'
+					'". $url."',
+					'". $timeTaken."',
+					'". $ip."',
+					'". $os."'
 					);";
 
 $user = mysqli_query ($db_handle, $sql);
