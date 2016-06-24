2f2f58b49887773e7cf1f79599766fb17556449b
rahullahoria <rahul_lahoria@yahoo.com>
2016-06-24 14:22:31 +0530

    added get worker by id

--- apis/inc_workers/worker.php
+++ apis/inc_workers/worker.php
@@ -1,8 +1,14 @@
 <?php
 
-$sql = "SELECT u.id as id, u.name, u.mobile, w.id as worker_id , w.emergency_no
+if(isset($_GET['user_id'])) {
+    $sql = "SELECT u.id as id, u.name, u.mobile, w.id as worker_id , w.emergency_no
 FROM `bluenet_v3`.`workers` as w inner join `bluenet_v3`.users as u
-WHERE w.`ref_id` =" . $_GET['user_id'] ." and w.user_id = u.id  ";
+WHERE w.`ref_id` =" . $_GET['user_id'] . " and w.user_id = u.id  ";
+}
+
+if(isset($route[2]))
+    $sql = "SELECT  `name` , `mobile` , `email`  , `address` , `area` ," .
+        " `rating`   FROM `bluenet_v3`.`users` WHERE id = '" . $route[2] . "'; ";
 
 $result = mysqli_query($db_handle, $sql);
 //echo $sql;
