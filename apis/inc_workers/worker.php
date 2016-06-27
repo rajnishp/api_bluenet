3ef944cdf93fe3e98608beec9a860d30b14df44b
rahullahoria <rahul_lahoria@yahoo.com>
2016-06-27 14:46:26 +0530

    added update md4

--- apis/inc_workers/worker.php
+++ apis/inc_workers/worker.php
@@ -1,16 +1,18 @@
 <?php
 
 if(isset($_GET['user_id'])) {
-    $sql = "SELECT u.id AS id, u.name, u.mobile, u.rating, u.photo, w.id AS worker_id, w.emergency_no, ud.adhar_card, ud.voter_id, ud.driving_license, ud.pan_card
-FROM `bluenet_v3`.`workers` AS w
-LEFT JOIN `bluenet_v3`.users AS u ON w.user_id = u.id
+    $sql = "SELECT u.id AS id, u.name, u.mobile, u.rating, u.photo, u.`address` , u.`area` , w.id AS worker_id, w.emergency_no,".
+        " ud.adhar_card, ud.voter_id, ud.driving_license, ud.pan_card, w.`status`,w.`native_place`,w.`dob` ".
+        " FROM `bluenet_v3`.`workers` AS w".
+        " LEFT JOIN `bluenet_v3`.users AS u ON w.user_id = u.id
 LEFT JOIN `bluenet_v3`.user_documents AS ud ON u.id = ud.user_id
 WHERE w.`ref_id` =". $_GET['user_id'];
 }
 
 if(isset($route[2]))
-    $sql = "SELECT  `name` , `mobile` , `email`  , `address` , `area` ," .
-        " `rating`, `photo`   FROM `bluenet_v3`.`users`  WHERE md5_id = '" . $route[2] . "'; ";
+    $sql = "SELECT  u.`name` , u.`mobile` , u.`email`  , u.`address` , u.`area` , " .
+        " u.`rating`, u.`photo`, w.`status`, w.`emergency_no`,w.`native_place`,w.`dob` ".
+        "FROM `bluenet_v3`.`users` as u left join `bluenet_v3`.`workers` AS w ON w.user_id = u.id WHERE md5_id = '" . $route[2] . "'; ";
 
 $result = mysqli_query($db_handle, $sql);
 //echo $sql;
