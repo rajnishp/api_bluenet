af1fe57dd42dd12d110f903171b209b7af8cf18d
rahullahoria <rahul_lahoria@yahoo.com>
2016-09-03 14:39:43 +0530

    payment

--- apis/inc_society/addWorker.php
+++ apis/inc_society/addWorker.php
@@ -11,7 +11,7 @@ $input = json_decode(file_get_contents("php://input"));
 function createCustomer($db_handle,$name,$mobile,$address,$society){
 
 
-    $sql = "INSERT INTO `users` ( `id` , `name` , `mobile` , `email` , `password` , `type` , `address` , `area` ," .
+    $sql = "INSERT INTO `bluenet_v3`.`users` ( `id` , `name` , `mobile` , `email` , `password` , `type` , `address` , `area` ," .
         " `creation` ,  `society_id`  )
 			VALUES (NULL ,
 			'" . $name . "',
@@ -45,7 +45,7 @@ function createCustomerWorker($db_handle,$name,$mobile,$address,$photo,$refId,$l
 
 
     //1. adding user worker
-    $sql = "INSERT INTO `users` ( `id` , `name` , `mobile` , `email` , `password` , `type` , `address` , `area` ," .
+    $sql = "INSERT INTO `bluenet_v3`.`users` ( `id` , `name` , `mobile` , `email` , `password` , `type` , `address` , `area` ," .
         " `creation` , society_id, photo )
 			VALUES (NULL ,
 			'" . $name . "',
@@ -66,10 +66,10 @@ function createCustomerWorker($db_handle,$name,$mobile,$address,$photo,$refId,$l
 
 
     //2. updating md5 id
-    $result = mysqli_query($db_handle, "Update users set md5_id = MD5(".$uwId .") where id = ".$uwId );
+    $result = mysqli_query($db_handle, "Update `bluenet_v3`.users set md5_id = MD5(".$uwId .") where id = ".$uwId );
 
     //3. adding worker docs
-    $sql = "INSERT INTO `user_documents` (`id`,  `user_id`, `pv`, `adhar_card`, `voter_id`, `driving_license`, `pan_card`)
+    $sql = "INSERT INTO `bluenet_v3`.`user_documents` (`id`,  `user_id`, `pv`, `adhar_card`, `voter_id`, `driving_license`, `pan_card`)
 				VALUES (NULL,
 					'" . $uwId . "',
 					'" . $pv . "',
@@ -84,7 +84,7 @@ function createCustomerWorker($db_handle,$name,$mobile,$address,$photo,$refId,$l
     //4. adding worker
     /*INSERT INTO `bluenet_v3`.`workers` (`id`, `ref_id`, `user_id`, `status`, `emergency_no`, `native_place`, `native_add`, `dob`, `education`, `experience`, `gender`, `remark`, `salary`, `bonus`) VALUES
     (NULL, '1', '3', 'new', '9090909090', 'delhi', 'asdf', '2016-04-05', '10', '5', 'M', 'afsdv', '1000', '2');*/
-    $sql = "INSERT INTO `workers` (`id`, `ref_id`, `user_id`, `status`, `emergency_no`, `native_place`,
+    $sql = "INSERT INTO `bluenet_v3`.`workers` (`id`, `ref_id`, `user_id`, `status`, `emergency_no`, `native_place`,
 								`native_add`, `dob`, `education`, `experience`, `gender`, `remark`, `salary`, `local_id`)
 				VALUES (NULL,
 					'" . $refId . "',
@@ -108,7 +108,7 @@ function createCustomerWorker($db_handle,$name,$mobile,$address,$photo,$refId,$l
     $sId = array('maid'=> 1,'cook' => 2, 'car cleaner' => 15 );
 
     //5. adding worker service
-    $sql = "INSERT INTO `service_worker_mappings`
+    $sql = "INSERT INTO `bluenet_v3`.`service_worker_mappings`
 				(`id`, `worker_id`, `service_id`)
 					VALUES ('',
 					'" . $wId . "',
@@ -118,7 +118,7 @@ function createCustomerWorker($db_handle,$name,$mobile,$address,$photo,$refId,$l
     mysqli_query($db_handle, $sql);
 
     //6. adding worker society mapping
-    $sql = "INSERT INTO `society_worker_mapping`
+    $sql = "INSERT INTO `bluenet_v3`.`society_worker_mapping`
 				(`worker_id`, `society_id`)
 					VALUES (
 					'" . $wId . "',
