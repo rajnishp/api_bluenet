fc25ada0d67ee1608018da85d47dc8d3b1c76059
rahullahoria <rahul_lahoria@yahoo.com>
2016-09-03 15:24:04 +0530

    payment

--- apis/inc_society/addWorker.php
+++ apis/inc_society/addWorker.php
@@ -64,8 +64,8 @@ function createCustomerWorker($db_handle,$name,$mobile,$address,$photo,$refId,$l
 
     $uwId = mysqli_insert_id($db_handle);
 
-    if($uwId == 0){
-        echo " Error: " . mysqli_error($db_handle);
+    if($uwId == 0 || $uwId == null){
+        echo " Error: " . mysqli_error($db_handle);die();
     }
 
 
