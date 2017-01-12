ddfbaa95654183524fdb6e5eeac6d472ea98fedf
anil-ninja <kumar.anil8892@gmail.com>
2017-01-12 17:21:33 +0530

    count api

---
+++ apis/app_rec/count.php
@@ -0,0 +1,24 @@
+<?php
+
+$input = json_decode(file_get_contents ("php://input"));
+
+$location = "SELECT count(id) as count from cities WHERE 1 ;";
+$locationData = mysqli_query ($db_handle_bt_sp, $location);
+$data['location'] = $locationData['count'];
+
+$workerSp = "SELECT count(id) as count from service_providers WHERE 1 ;";
+$workerDataSp = mysqli_query ($db_handle_bt_sp, $workerSp);
+$worker = "SELECT count(id) as count from bluenet_v3.workers WHERE 1 ;";
+$workerData = mysqli_query ($db_handle, $worker);
+$data['worker'] = ($workerDataSp['count'] + $workerData['count']);
+
+$user = "SELECT count(id) as count from bluenet_v3.users WHERE type = 'customer' ;";
+$userData = mysqli_query ($db_handle, $user);
+$data['user'] = $userData['count'];
+
+$request = "SELECT count(id) as count from bluenet_v3.service_request WHERE 1 ;";
+$requestData = mysqli_query ($db_handle, $request);
+$data['request'] = $requestData['count'];
+
+echo '{"root":'.json_encode($rows).'}';
+?>
